f922c1be31e98d3c3ff7e4f6d9620e8556356e83
GarreyXu <garreyxu@gmail.com>
2021-02-11 23:18:19 -0800

    subset

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CrackingCodingInterview
 {
@@ -6,11 +7,31 @@ namespace CrackingCodingInterview
     {
         static void Main(string[] args)
         {
-            string input1 = "aacecaaa";
-            string input2 = "dcbabcd";
+            int[] nums1 = {1,2,3};
+            int[] nums2 = {0};
 
-            Console.Out.WriteLine(ShortestPalindroneSolution.ShortestPalindrome(input1));
-            Console.Out.WriteLine(ShortestPalindroneSolution.ShortestPalindrome(input2));
+            IList<IList<int>> result1 = SubsetSolution.Subsets(nums1);
+            IList<IList<int>> result2 = SubsetSolution.Subsets(nums2);
+ 
+            foreach(var list in result1)
+            {
+                Console.Out.Write("[");
+                foreach(var res in list)
+                {
+                    Console.Out.Write(res + ",");
+                }
+                Console.Out.Write("]");
+            }
+            Console.Out.WriteLine("divider");
+            foreach(var list in result2)
+            {
+                Console.Out.Write("[");
+                foreach(var res in list)
+                {
+                    Console.Out.Write(res);
+                }
+                Console.Out.Write("]");
+            }
         }
     }
 }
